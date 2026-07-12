7967128ce6be59c4bccb13aa7341d2be40490cf7
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,34 @@
-output "data_factory_linked_service_azure_table_storages" {
-  description = "All data_factory_linked_service_azure_table_storage resources"
-  value       = azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages
-  sensitive   = true
-}
 output "data_factory_linked_service_azure_table_storages_additional_properties" {
-  description = "List of additional_properties values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.additional_properties]
+  description = "Map of additional_properties values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.additional_properties }
 }
 output "data_factory_linked_service_azure_table_storages_annotations" {
-  description = "List of annotations values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.annotations]
+  description = "Map of annotations values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.annotations }
 }
 output "data_factory_linked_service_azure_table_storages_connection_string" {
-  description = "List of connection_string values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.connection_string]
+  description = "Map of connection_string values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.connection_string }
   sensitive   = true
 }
 output "data_factory_linked_service_azure_table_storages_data_factory_id" {
-  description = "List of data_factory_id values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.data_factory_id]
+  description = "Map of data_factory_id values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.data_factory_id }
 }
 output "data_factory_linked_service_azure_table_storages_description" {
-  description = "List of description values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.description]
+  description = "Map of description values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.description }
 }
 output "data_factory_linked_service_azure_table_storages_integration_runtime_name" {
-  description = "List of integration_runtime_name values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.integration_runtime_name]
+  description = "Map of integration_runtime_name values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.integration_runtime_name }
 }
 output "data_factory_linked_service_azure_table_storages_name" {
-  description = "List of name values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.name]
+  description = "Map of name values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.name }
 }
 output "data_factory_linked_service_azure_table_storages_parameters" {
-  description = "List of parameters values across all data_factory_linked_service_azure_table_storages"
-  value       = [for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : v.parameters]
+  description = "Map of parameters values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.parameters }
 }
 
