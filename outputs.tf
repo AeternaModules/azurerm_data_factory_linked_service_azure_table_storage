a1000b03fd62d1605007c037afd2b4f07442e08e
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:01:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "data_factory_linked_service_azure_table_storages_id" {
+  description = "Map of id values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.id }
+}
 output "data_factory_linked_service_azure_table_storages_additional_properties" {
   description = "Map of additional_properties values across all data_factory_linked_service_azure_table_storages, keyed the same as var.data_factory_linked_service_azure_table_storages"
   value       = { for k, v in azurerm_data_factory_linked_service_azure_table_storage.data_factory_linked_service_azure_table_storages : k => v.additional_properties }
